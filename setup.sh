1dff0c258f1000181af4c3e11ca6116a71e04099
Hyeonseo <54432870+hyeonseo0128@users.noreply.github.com>
2022-04-04 13:17:12 +0900

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get -y install python3-pip
+sudo apt-get -y install curl
+sudo apt-get -y install nano
+
+sudo apt install -y nvidia-jetpack
+sudo apt-get install -y libopenblas-base libopenmpi-dev
+
+pip3 install Cython
+pip3 install numpy
+
+wget https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+pip3 install torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+
+git clone --branch v0.11.1 https://github.com/pytorch/vision torchvision
+sudo python3 torchvision/setup.py install
+
+git clone https://github.com/ultralytics/yolov5.git
+pip3 install -r yolov5/requirements.txt
